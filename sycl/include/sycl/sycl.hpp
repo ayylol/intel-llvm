aeec02cf95abd00ccf0c98b52b42e780c9b59cf1
Garcia Orozco, David <david.garcia.orozco@intel.com>
2025-06-03 13:12:31 -0700

    Remove annotated_arg extension

--- sycl/include/sycl/sycl.hpp
+++ sycl/include/sycl/sycl.hpp
@@ -79,7 +79,6 @@
 #include <sycl/ext/oneapi/device_global/device_global.hpp>
 #include <sycl/ext/oneapi/device_global/properties.hpp>
 #include <sycl/ext/oneapi/experimental/address_cast.hpp>
-#include <sycl/ext/oneapi/experimental/annotated_arg/annotated_arg.hpp>
 #include <sycl/ext/oneapi/experimental/annotated_ptr/annotated_ptr.hpp>
 #include <sycl/ext/oneapi/experimental/annotated_usm/alloc_device.hpp>
 #include <sycl/ext/oneapi/experimental/annotated_usm/alloc_host.hpp>
